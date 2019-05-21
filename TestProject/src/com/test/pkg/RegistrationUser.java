067792387f35521f749af58d2589c2dfca1e85db
Prateek <Prateek@DESKTOP-N6T2AN7>
2019-05-20 21:54:51 -0500

    Second Commit

---
+++ TestProject/src/com/test/pkg/RegistrationUser.java
@@ -0,0 +1,12 @@
+package com.test.pkg;
+
+public class RegistrationUser {
+
+	public String userName;
+	public String emailId;
+	public RegistrationUser(String userName, String emailId) {
+		super();
+		this.userName = userName;
+		this.emailId = emailId;
+	}
+}
